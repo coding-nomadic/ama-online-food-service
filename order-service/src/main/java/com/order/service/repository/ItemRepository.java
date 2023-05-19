6a089251cdae04373c6d1ba77a5cc93c80d44f8d
Tenzin Dawa <tnzngdw@gmail.com>
2023-05-19 15:29:17 -0400

    Update ItemRepository.java

--- order-service/src/main/java/com/order/service/repository/ItemRepository.java
+++ order-service/src/main/java/com/order/service/repository/ItemRepository.java
@@ -4,11 +4,8 @@ import com.order.service.entities.Item;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
-import java.util.Optional;
-
 @Repository
 public interface ItemRepository extends CrudRepository<Item,Long> {
 
-    Optional<Item> findByName(String name);
+    Boolean existsByName(String name);
 }
