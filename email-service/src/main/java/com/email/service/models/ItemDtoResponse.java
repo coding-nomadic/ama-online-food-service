5b045772fad2e4b2f241b02b8d3dc1d88b6218be
Tenzin Dawa <tnzngdw@gmail.com>
2023-05-20 12:27:36 -0400

    Add files via upload

---
+++ email-service/src/main/java/com/email/service/models/ItemDtoResponse.java
@@ -0,0 +1,13 @@
+package com.email.service.models;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class ItemDtoResponse implements Serializable {
+    private Long id;
+    private String name;
+    private String quantity;
+    private Long price;
+}
