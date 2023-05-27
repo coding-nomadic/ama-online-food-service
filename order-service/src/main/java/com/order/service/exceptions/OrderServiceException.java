be47afe9b037d0f45f90632832e936f6e61eb75b
Tenzin Dawa <tnzngdw@gmail.com>
2023-05-27 10:38:23 -0400

    Update OrderServiceException.java

--- order-service/src/main/java/com/order/service/exceptions/OrderServiceException.java
+++ order-service/src/main/java/com/order/service/exceptions/OrderServiceException.java
@@ -5,14 +5,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class OrderServiceException extends RuntimeException {
-    private String message;
-    private String errorCode;
+
     public OrderServiceException(String message, Throwable cause) {
         super(message, cause);
     }
     public OrderServiceException(String message, String errorCode) {
         super(message);
-        this.message = message;
-        this.errorCode = errorCode;
     }
 }
