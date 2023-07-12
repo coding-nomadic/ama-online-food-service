fd574b7c1a0defd29ed9f7c70f93281b59890b01
Tenzin Dawa <tnzngdw@gmail.com>
2023-07-12 13:42:19 -0400

    Update MenuService.java

--- order-service/src/main/java/com/order/service/services/MenuService.java
+++ order-service/src/main/java/com/order/service/services/MenuService.java
@@ -1,38 +1,46 @@
 package com.order.service.services;
 
-
-import com.order.service.entities.Menu;
-import com.order.service.models.MenuDto;
-import com.order.service.models.MenuDtoResponse;
+import com.order.service.entities.Order;
+import com.order.service.exceptions.OrderServiceException;
+import com.order.service.models.OrderDto;
+import com.order.service.models.OrderDtoResponse;
+import com.order.service.publisher.MessagePublisher;
 import com.order.service.repository.MenuRepository;
+import com.order.service.repository.OrderRepository;
+import com.order.service.utils.GenericUtils;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
-
 @Service
-public class MenuService {
+public class OrderService {
 
-    private MenuRepository menuRepository;
 
     private ModelMapper modelMapper;
+    private OrderRepository orderRepository;
+
+    private MenuRepository itemRepository;
+
+    private MessagePublisher messagePublisher;
 
     @Autowired
-    public MenuService(MenuRepository menuRepository, ModelMapper modelMapper) {
+    public OrderService(MessagePublisher messagePublisher, MenuRepository itemRepository, OrderRepository orderRepository, ModelMapper modelMapper) {
+        this.itemRepository = itemRepository;
+        this.orderRepository = orderRepository;
         this.modelMapper = modelMapper;
-        this.menuRepository = menuRepository;
+        this.messagePublisher = messagePublisher;
     }
 
-    public MenuDtoResponse createMenu(MenuDto menuDto) {
-        Menu itemResponse = menuRepository.save(modelMapper.map(menuDto, Menu.class));
-        return modelMapper.map(itemResponse, MenuDtoResponse.class);
-    }
-
-    public List<MenuDtoResponse> fetchMenu() {
-        final Iterable<Menu> items = menuRepository.findAll();
-        return StreamSupport.stream(items.spliterator(), false).map(m -> modelMapper.map(m, MenuDtoResponse.class)).collect(Collectors.toList());
+    public OrderDtoResponse createOrders(OrderDto orderDto) {
+        orderDto.setStatus("accepted");
+        orderDto.getItems().forEach(s -> {
+            if (!itemRepository.existsByName(s.getName())) {
+                throw new OrderServiceException("Item not found for " + s.getName(), "102");
+            }
+        });
+        Order order = orderRepository.save(modelMapper.map(orderDto, Order.class));
+        OrderDtoResponse orderDtoResponse = GenericUtils.orderResponse(order, modelMapper);
+        messagePublisher.publishMessage(orderDtoResponse);
+        return orderDtoResponse;
     }
 }
