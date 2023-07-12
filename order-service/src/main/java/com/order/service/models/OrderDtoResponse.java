8357b48f5e1f6d75cbda15f7f7f98316e29377f4
Tenzin Dawa <tnzngdw@gmail.com>
2023-07-12 13:48:09 -0400

    Update OrderDtoResponse.java

--- order-service/src/main/java/com/order/service/models/OrderDtoResponse.java
+++ order-service/src/main/java/com/order/service/models/OrderDtoResponse.java
@@ -2,12 +2,13 @@ package com.order.service.models;
 
 
 import lombok.Data;
-
+import lombok.Builder;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 
 @Data
+@Builder
 public class OrderDtoResponse implements Serializable {
     private Long id;
     private String status;
