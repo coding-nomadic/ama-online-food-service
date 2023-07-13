d09e242458b9c33993fbe1318a061bf839a7d324
Tenzin Dawa <tnzngdw@gmail.com>
2023-07-13 10:03:07 -0400

    Update MenuController.java

--- order-service/src/main/java/com/order/service/controllers/MenuController.java
+++ order-service/src/main/java/com/order/service/controllers/MenuController.java
@@ -5,6 +5,7 @@ import com.order.service.models.MenuDto;
 import com.order.service.models.MenuDtoResponse;
 import com.order.service.services.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,5 +35,11 @@ public class MenuController {
     @GetMapping(path = "/{id}")
     public ResponseEntity<MenuDtoResponse> fetchById(@PathVariable String id) {
         return ResponseEntity.ok(menuService.fetchMenu(id));
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable String id) {
+        menuService.deleteMenu(id);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
