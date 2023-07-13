db8d91f3f19db9fb9ad998e97678a7a1f14db6e2
Tenzin Dawa <tnzngdw@gmail.com>
2023-07-13 13:50:56 -0400

    Update Menu.java

--- order-service/src/main/java/com/order/service/entities/Menu.java
+++ order-service/src/main/java/com/order/service/entities/Menu.java
@@ -1,24 +1,18 @@
 package com.order.service.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
-
 import javax.persistence.*;
 
 @Entity
 @Table(name = "tbl_menu")
 @Data
 public class Menu {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
     private Long id;
-
-    @Column(name = "name", nullable = false)
     private String name;
-
-    @Column(name = "quantity", nullable = false)
     private String quantity;
-
-    @Column(name = "price", nullable = false)
     private Long price;
 }
