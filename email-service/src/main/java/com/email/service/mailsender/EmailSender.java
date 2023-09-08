55157f86dd5c44749738e4d99c6767a9d13eda54
Tenzin Dawa <tnzngdw@gmail.com>
2023-09-07 21:32:20 -0400

    Update EmailSender.java

--- email-service/src/main/java/com/email/service/mailsender/EmailSender.java
+++ email-service/src/main/java/com/email/service/mailsender/EmailSender.java
@@ -1,39 +1,35 @@
-package com.email.service.mailsender;
-
-import com.email.service.models.OrderDtoResponse;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Component;
-
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Component
 @Slf4j
 public class EmailSender {
 
-    @Autowired
-    private JavaMailSender mailSender;
+    private final JavaMailSender mailSender;
     private static final String SENDER = "tnzngdw@gmail.com";
     private static final String UTF = "utf-8";
+
+    @Autowired
+    public EmailSender(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
 
     @Async
     public void sendMail(OrderDtoResponse orderDtoResponse) {
         try {
-            List<Long> prices = orderDtoResponse.getItems().stream().map(s -> Long.valueOf(s.getPrice())).collect(Collectors.toList());
-            Long totalPrices = prices.stream().collect(Collectors.summingLong(Long::intValue));
-            String emailText = "Thank you for ordering from Ama Food. Your order ID " + orderDtoResponse.getId() + " amounting $" + totalPrices + " CAD will be delivered shortly !";
+            List<Long> prices = orderDtoResponse.getItems().stream()
+                    .map(s -> Long.valueOf(s.getPrice()))
+                    .collect(Collectors.toList());
+
+            Long totalPrices = prices.stream()
+                    .collect(Collectors.summingLong(Long::intValue));
+
+            String emailText = "Thank you for ordering from Ama Food. Your order ID " + orderDtoResponse.getId() +
+                    " amounting $" + totalPrices + " CAD will be delivered shortly!";
+
             MimeMessage mimeMessage = getMimeMessage(emailText, orderDtoResponse);
             mailSender.send(mimeMessage);
-            log.info("Email sent to the customer successfully !");
+            log.info("Email sent to the customer successfully for order ID: {}", orderDtoResponse.getId());
         } catch (MessagingException e) {
-            log.error("Failed to send email for: " + orderDtoResponse.getEmail() + "\n" + e.getLocalizedMessage());
-            throw new IllegalArgumentException("Failed to send email for: " + orderDtoResponse.getEmail());
+            log.error("Failed to send email for order ID: {}", orderDtoResponse.getId(), e);
+            throw new IllegalArgumentException("Failed to send email for order ID: " + orderDtoResponse.getId(), e);
         }
     }
 
@@ -47,5 +43,3 @@ public class EmailSender {
         return mimeMessage;
     }
 }
-
-
