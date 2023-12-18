ad65126b94e638cf73d952af3aec3f4f77706d4e
Tenzin Dawa <tnzngdw@gmail.com>
2023-12-17 23:32:12 -0500

    Update KafkaTopicConfig.java

--- order-service/src/main/java/com/order/service/configurations/KafkaTopicConfig.java
+++ order-service/src/main/java/com/order/service/configurations/KafkaTopicConfig.java
@@ -1,1 +1,17 @@
+package com.order.service.configurations;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+    @Value("${order.topic.name}")
+    String orderConfirmationTopic;
+    @Bean
+    public NewTopic orderTopicConfig() {
+        return TopicBuilder.name(orderConfirmationTopic).build();
+    }
+}
